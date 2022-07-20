42cb9ee52da6d053ea490b1d44c58e5638400882
Dmitriy Dorosh <doroshdmitriy@gmail.com>
2022-07-20 00:20:47 -0700

    Finished sidebar implementation with routing as well as navbar

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ import './App.css';
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import { ECommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, 
   Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages'
+import { useStateContext } from './contexts/ContextProvider';
 
 const App = () => {
-  const activeMenu = true;
+  const { activeMenu } = useStateContext();
 
   return (
     <div>
